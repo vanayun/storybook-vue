4d754de144071541a532d846d0eebc7e3b94fe16
vanayun <eldaashe@naver.com>
2019-02-05 11:45:32 +0900

    init project

---
+++ src/stories/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="{ rounded: rounded }"><slot></slot></button>
+</template>
+
+<script>
+export default {
+	name: 'MyButton',
+  props: {
+    rounded: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style>
+.rounded {
+  border-radius: 5px;
+}
+</style>
